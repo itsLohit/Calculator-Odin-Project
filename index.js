989e43080c5ce46d6c069a73609d11039c3be49d
itsLohit <109834876+itsLohit@users.noreply.github.com>
2024-07-18 12:02:16 +0530

    Added CSS Styles and Calulation Logics

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
-let currentNumber = '';
+let previousScreenValue = "";
+let currentScreenValue = "";
+let operator = "";
+let opCount = 0;
+
 
 
 
 document.addEventListener("DOMContentLoaded", function(){
-    const btnNumbers=document.querySelectorAll('.num');
-    const btnOperators=document.querySelectorAll('.op');
+    const btnNumbers=document.querySelectorAll(".num");
+    const btnOperators=document.querySelectorAll(".op");
 
-    const btnEqual=document.querySelector('#equals');
-    const btnClear=document.querySelector('#clear');
+    const btnEqual=document.querySelector("#equals");
+    const btnClear=document.querySelector("#clear");
     const btnDot=document.querySelector("#btnDot");
 
     const previousScreen=document.querySelector(".previous");
     const currentScreen=document.querySelector(".current");
 
-    btnNumbers.forEach((num)=>num.addEventListener("click", function(e){
-        handleNumber(e.target.textContent)
-        currentScreen.textContent = currentNumber;
-    }))
+    btnNumbers.forEach((num) => num.addEventListener("click", (e)=>{
+        if(currentScreenValue.length < 7){
+            currentScreenValue += e.target.textContent;
+        }
+        currentScreen.textContent = currentScreenValue;
+    }));
 
-});
+    btnOperators.forEach((op)=>op.addEventListener("click", (e)=>{
+        
+        if(opCount>0){
+            console.log("Success");
+            operator = e.target.textContent;
+            currentScreen.textContent = previousScreenValue+ " " + operator;
+
+        }
+        opCount+=1;
+        operator = e.target.textContent;
+        previousScreenValue = currentScreenValue;
+        currentScreenValue = "";
+        currentScreen.textContent = "";
+        previousScreen.textContent = previousScreenValue+ " " +operator; 
+        console.log("opCount"+opCount)
+    }));
+
+    btnEqual.addEventListener("click", ()=>{
+        previousScreen.textContent = previousScreenValue + " " + operator + " " + currentScreenValue;
+        previousScreenValue = Number(previousScreenValue);
+        currentScreenValue = Number(currentScreenValue);
+        previousScreenValue = operate(previousScreenValue, operator, currentScreenValue);
+        currentScreen.textContent = previousScreenValue;
+        console.log(previousScreenValue);
+    });
 
 
-function handleNumber(num){
-    if(currentNumber.length <7){
-        currentNumber += num;
-    }
+    btnClear.addEventListener("click", ()=>{
+        previousScreenValue = "";
+        currentScreenValue = "";
+        operator = "";
+        currentScreen.textContent = currentScreenValue;
+        previousScreen.textContent = currentScreenValue;
+        
+    })
+
     
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let operand1 = null;
-let operand2 = null;
-let operator = null;
-
-
-
-
-
-
-
-
+});
 
 
 
@@ -64,16 +72,16 @@ let operator = null;
 
 
 function operate(operand1, operator, operand2){
-    if(operator==='+'){
+    if(operator==="+"){
         return addNumbers(operand1, operand2);
     }
-    else if(operator==='-'){
+    else if(operator==="-"){
         return subtractNumbers(operand1, operand2);
     }
-    else if(operator==='*'){
+    else if(operator==="*"){
         return multiplyNumbers(operand1, operand2);
     }
-    else if(operator==='/'){
+    else if(operator==="/"){
         return divideNumbers(operand1, operand2);
     }
 }
@@ -96,15 +104,8 @@ function divideNumbers(operand1, operand2){
     return operand1/operand2;
 }
 
-operand1 = 1000;
-operand2 = 2000;
-operator = '/';
 
-console.log(addNumbers(operand1, operand2));
-console.log(subtractNumbers(operand1, operand2));
-console.log(multiplyNumbers(operand1, operand2));
-console.log(divideNumbers(operand1, operand2));
 
-console.log(operate(operand1, operator, operand2));
+
 
 
