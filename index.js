7dd47ee7b72a331a232d9310cddddc03dfa7f03e
itsLohit <109834876+itsLohit@users.noreply.github.com>
2024-07-18 20:47:49 +0530

    Created UI for the Calculator

--- index.js
+++ index.js
@@ -23,26 +23,72 @@ document.addEventListener("DOMContentLoaded", function(){
     
 
     btnNumbers.forEach((num) => num.addEventListener("click", (e)=>{
-        if(currentScreenValue.length < 9){
-            currentScreenValue += e.target.textContent;
-        }
-        currentScreen.textContent = currentScreenValue;
+        handleNumbers(e.target.textContent);
     }));
 
     btnDot.addEventListener("click", (e)=>{
-        if(dotIndicator==0){
-            currentScreenValue += e.target.textContent;
-            currentScreen.textContent = currentScreenValue;
-            dotIndicator = 1;
-        }
+        handleDot(e.target.textContent);
     });
 
     btnOperators.forEach((op)=>op.addEventListener("click", (e)=>{
+        handleOperators(e.target.textContent);
+    }));
+
+    btnEqual.addEventListener("click", ()=>{
+        handleEquals();
+    });
+
+    btnClear.addEventListener("click", ()=>{
+        handleClear();
+    });
+
+    btnBS.addEventListener("click", ()=>{
+        handleBackspace();
+    });
+
+
+
+    window.addEventListener("keydown", e => {
+        const key = e.key;
+        if (!isNaN(key)){
+            handleNumbers(key);
+        }
+        else if (key === '+' || key === '-' || key === '*' || key === '/'){
+            handleOperators(key);
+        }
+        else if (key === 'Enter'){
+            handleEquals(key);
+        }
+        else if (key === 'Backspace'){
+            handleBackspace(key);
+        } 
+        else if (key==='Escape'){
+            handleClear(key);
+        }
+        else if (key === '.'){
+            handleDot(key);
+        }
+        else if (key === 'Delete'){
+            handleClear(key);
+        }
+    });
+
+
+
+    function handleNumbers(num){
+        if(currentScreenValue.length < 9){
+            currentScreenValue += num;
+        }
+        currentScreen.textContent = currentScreenValue;
+    }
+    
+    
+    function handleOperators(op){
 
         dotIndicator = 0;
         
         if((equalsIndicator==0) && (operatorIndicator==0)){
-            operator = e.target.textContent;
+            operator = op;
             previousScreenValue = currentScreenValue;
             currentScreenValue = "";
             currentScreen.textContent = "";
@@ -58,7 +104,7 @@ document.addEventListener("DOMContentLoaded", function(){
             previousScreenValue = operate(previousScreenValue, operator, currentScreenValue);
             currentScreenValue = "";
             currentScreen.textContent = currentScreenValue;
-            operator = e.target.textContent;
+            operator = op;
             previousScreen.textContent = previousScreenValue + " " + operator;
             if (previousScreenValue>999999999 || currentScreenValue>999999999){
                 previousScreen.textContent = "Can't ";
@@ -66,7 +112,7 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         }
         else if((equalsIndicator>0) && (operatorIndicator>0)){
-            operator = e.target.textContent;
+            operator = op;
             previousScreen.textContent = previousScreenValue + " " + operator;
             currentScreenValue = "";
             currentScreen.textContent = currentScreenValue;
@@ -79,16 +125,17 @@ document.addEventListener("DOMContentLoaded", function(){
             previousScreenValue = operate(previousScreenValue, operator, currentScreenValue);
             currentScreenValue = "";
             currentScreen.textContent = currentScreenValue;
-            operator = e.target.textContent;
+            operator = op;
             previousScreen.textContent = previousScreenValue + " " + operator;
             if (previousScreenValue>999999999 || currentScreenValue>999999999){
                 previousScreen.textContent = "Can't ";
                 currentScreen.textContent = "hold!";
             }
         }
-    }));
-
-    btnEqual.addEventListener("click", ()=>{
+    }
+    
+    
+    function handleEquals(){
         if((previousScreenValue != "") && (currentScreenValue != "")){
             previousScreen.textContent = previousScreenValue+operator+currentScreenValue;
             previousScreenValue = operate(previousScreenValue, operator, currentScreenValue);
@@ -99,11 +146,19 @@ document.addEventListener("DOMContentLoaded", function(){
                 currentScreen.textContent = "hold!";
             }
         }
-        
-    });
-
-
-    btnClear.addEventListener("click", ()=>{
+    }
+    
+    
+    function handleDot(dot){
+        if(dotIndicator==0){
+            currentScreenValue += dot;
+            currentScreen.textContent = currentScreenValue;
+            dotIndicator = 1;
+        }
+    }
+    
+    
+    function handleClear(){
         previousScreenValue = "";
         currentScreenValue = "";
         operator = "";
@@ -112,100 +167,14 @@ document.addEventListener("DOMContentLoaded", function(){
         dotIndicator = 0;
         currentScreen.textContent = currentScreenValue;
         previousScreen.textContent = currentScreenValue;
-    });
+    }
 
-    btnBS.addEventListener("click", ()=>{
+
+    function handleBackspace(){
         currentScreenValue = currentScreenValue.slice(0, currentScreenValue.length - 1);
         currentScreen.textContent = currentScreenValue;
-    })
-
-    window.addEventListener("keydown", e => {
-        const key = e.key;
-        if (!isNaN(key) || key === '.') {
-            if(currentScreenValue.length < 9){
-                currentScreenValue += String(key);
-            }
-            currentScreen.textContent = currentScreenValue;
-        }
-        else if (key === '+' || key === '-' || key === '*' || key === '/') {
-            dotIndicator = 0;
-        
-        if((equalsIndicator==0) && (operatorIndicator==0)){
-            operator = key;
-            previousScreenValue = currentScreenValue;
-            currentScreenValue = "";
-            currentScreen.textContent = "";
-            previousScreen.textContent = previousScreenValue+ " " +operator;
-            operatorIndicator = 1; 
-            console.log(operatorIndicator);
-            if (previousScreenValue>999999999 || currentScreenValue>999999999){
-                previousScreen.textContent = "Can't ";
-                currentScreen.textContent = "hold!";
-            }
-        }
-        else if((operatorIndicator==1) && (equalsIndicator==1)){
-            previousScreenValue = operate(previousScreenValue, operator, currentScreenValue);
-            currentScreenValue = "";
-            currentScreen.textContent = currentScreenValue;
-            operator = key;
-            previousScreen.textContent = previousScreenValue + " " + operator;
-            if (previousScreenValue>999999999 || currentScreenValue>999999999){
-                previousScreen.textContent = "Can't ";
-                currentScreen.textContent = "hold!";
-            }
-        }
-        else if((equalsIndicator>0) && (operatorIndicator>0)){
-            operator = key;
-            previousScreen.textContent = previousScreenValue + " " + operator;
-            currentScreenValue = "";
-            currentScreen.textContent = currentScreenValue;
-            if (previousScreenValue>999999999 || currentScreenValue>999999999){
-                previousScreen.textContent = "Can't ";
-                currentScreen.textContent = "hold!";
-            }
-        }
-        else if((operatorIndicator==1) && (equalsIndicator==0)){
-            previousScreenValue = operate(previousScreenValue, operator, currentScreenValue);
-            currentScreenValue = "";
-            currentScreen.textContent = currentScreenValue;
-            operator = key;
-            previousScreen.textContent = previousScreenValue + " " + operator;
-            if (previousScreenValue>999999999 || currentScreenValue>999999999){
-                previousScreen.textContent = "Can't ";
-                currentScreen.textContent = "hold!";
-            }
-        }
-        }
-        else if (key === 'Enter') {
-            e.preventDefault(); // Prevent form submission if inside a form
-            if((previousScreenValue != "") && (currentScreenValue != "")){
-                previousScreen.textContent = previousScreenValue+operator+currentScreenValue;
-                previousScreenValue = operate(previousScreenValue, operator, currentScreenValue);
-                currentScreen.textContent = previousScreenValue;
-                equalsIndicator =1;
-                if (previousScreenValue>999999999 || currentScreenValue>999999999){
-                    previousScreen.textContent = "Can't ";
-                    currentScreen.textContent = "hold!";
-                }
-            }
-        }
-        else if (key === 'Backspace') {
-            currentScreenValue = currentScreenValue.slice(0, currentScreenValue.length - 1);
-            currentScreen.textContent = currentScreenValue;
-        } 
-        else if (key==='Escape') {
-            previousScreenValue = "";
-            currentScreenValue = "";
-            operator = "";
-            operatorIndicator = 0;
-            equalsIndicator = 0;
-            dotIndicator = 0;
-            currentScreen.textContent = currentScreenValue;
-            previousScreen.textContent = currentScreenValue;
-        }
-
-    });
-
+    }
+    
     
 });
 
@@ -253,5 +222,19 @@ function divideNumbers(operand1, operand2){
 
 
 
+
+const button = document.querySelectorAll("button");
+
+button.forEach((btn)=>btn.addEventListener("mouseover", function () {
+
+btn.style.background = "#8458B3";
+btn.style.color = "#e5eaf5";
+}));
+
+button.forEach((btn)=>btn.addEventListener("mouseout", function () {
+
+    btn.style.background = "#d0bdf4";
+    btn.style.color = "black";
+    }));
 
 
